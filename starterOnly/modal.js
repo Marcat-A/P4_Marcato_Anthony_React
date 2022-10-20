364f2fbe0623430b0da62393482566fbb9bf5270
Marcat-A <anthony.marcato@gmail.com>
2022-10-20 16:24:48 +0200

    stable message errors

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,19 +26,42 @@ const onClose = () => {
   modalbg.style.display = "none";
 };
 
-// form
+// Get all the elements
 const form = document.getElementById("form");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birth = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
-const locations = document.getElementsByName("location");
+const radios = document.getElementsByName("location");
+const location6 = document.getElementById("location6");
 const utils = document.getElementById("checkbox1");
 
-// check from
+// Error Messages
+
+const errors = {
+  firstName: "Veuillez entrer un prénom valide.",
+  lastName: "Veuillez entrer un nom valide.",
+  email: "Veuillez entrer un email valide.",
+  location: "Veuillez selectionner une ville dans la liste.",
+  utils: "Veuillez accepter les conditions générales d'utilisation.",
+};
+
+// Call back the message
+
+const errorMessage = (e, message) => {
+  const div = document.createElement("div");
+  div.classList.add("form-alert");
+  div.innerHTML = message;
+  e.parentElement.append(div);
+};
+
+// Check the form
 
 form.addEventListener("submit", (e) => {
+  // Check if alredy an error, remove if alredy have one
+  document.querySelectorAll(".form-alert").forEach((e) => e.remove());
+
   // Create the email RegExp
   const regex = new RegExp(
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -49,26 +72,30 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   // If firstname not empty and do at least 2 chars
   if (!firstName.value || firstName.value.length <= 1) {
-    return false;
+    errorMessage(firstName, errors.firstName);
   }
   // If lastname not empty and do at least 2 chars
   if (!lastName.value || lastName.value.length <= 1) {
-    return false;
+    errorMessage(lastName, errors.lastName);
   }
   // If mail is valid
   if (!testEmail) {
-    return false;
+    errorMessage(email, errors.email);
   }
   // If a radio is checked
-  for (const radio of locations) {
-    if (radio.checked) {
-      break;
-    } else {
-      return false;
-    }
+  // for (const radio of radios) {
+  //   if (!radio.checked) {
+  //     errorMessage(radio, errors.location);
+  //     break;
+  //   }
+  // }
+
+  const radioChecked = document.querySelector('input[name="location"]:checked');
+  if (!radioChecked) {
+    errorMessage(location6, errors.location);
   }
   // If the utils are checked
   if (!utils.checked) {
-    return false;
+    errorMessage(utils, errors.utils);
   }
 });
