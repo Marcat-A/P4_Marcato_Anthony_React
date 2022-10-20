03e2e260994c0feb4659e8df1a103586058a9d37
Marcat-A <anthony.marcato@gmail.com>
2022-10-20 12:38:51 +0200

    tests ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,3 +25,50 @@ function launchModal() {
 const onClose = () => {
   modalbg.style.display = "none";
 };
+
+// form
+const form = document.getElementById("form");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birth = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const locations = document.getElementsByName("location");
+const utils = document.getElementById("checkbox1");
+
+// check from
+
+form.addEventListener("submit", (e) => {
+  // Create the email RegExp
+  const regex = new RegExp(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+  // Test if that's a valid mail
+  const testEmail = regex.test(email.value);
+  // No refresh of the page
+  e.preventDefault();
+  // If firstname not empty and do at least 2 chars
+  if (!firstName.value || firstName.value.length <= 1) {
+    return false;
+  }
+  // If lastname not empty and do at least 2 chars
+  if (!lastName.value || lastName.value.length <= 1) {
+    return false;
+  }
+  // If mail is valid
+  if (!testEmail) {
+    return false;
+  }
+  // If a radio is checked
+  for (const radio of locations) {
+    if (radio.checked) {
+      break;
+    } else {
+      return false;
+    }
+  }
+  // If the utils are checked
+  if (!utils.checked) {
+    return false;
+  }
+});
